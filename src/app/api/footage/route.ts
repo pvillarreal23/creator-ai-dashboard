5819bcb975b7b275345fe4c50514906edcbaa6c7
pvillarreal23 <pvillarreal23@gmail.com>
2026-04-02 19:58:22 -0500

    Update route.ts

--- src/app/api/footage/route.ts
+++ src/app/api/footage/route.ts
@@ -1,6 +1,5 @@
 export const dynamic = 'force-dynamic';
-
-export async function GET(request) {
+export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get('query') ?? 'ai tools';
   const per_page = searchParams.get('per_page') ?? '10';
